ada67a86499035550c6279336ec7323056abef9f
DevState <125184554+CrxyScripter@users.noreply.github.com>
2023-04-16 15:26:58 +0800

    Create Check.lua

---
+++ Games/Check.lua
@@ -0,0 +1,6 @@
+local Game = game.PlaceId
+if Game == 6284583030 or 10321372166 or 7722306047 then
+  loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/CrxyScripter/Zees/main/Games/Pet_Simulator_X.txt"))()
+else
+  game:GetService("Players").LocalPlayer.Kick("Unsupported Games!")
+end
